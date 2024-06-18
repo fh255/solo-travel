cfbf29a8a1da8c59c9387083c77eeef2be7c822a
Fatema Mahabub <fmahabub8@gmail.com>
2024-06-18 16:41:43 +0000

    add model for add post

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
 from .models import Comment
 from django import forms
 
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your post content here',
+        })
