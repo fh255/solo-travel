a7c0f72c9a11086cc445c3ea23268f1eb39ad72e
fh255 <34744096+fh255@users.noreply.github.com>
2024-06-28 23:23:45 +0200

    Delete env.py
    should not be committed, only for local used

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = os.getenv("DATABASE_URL")
-os.environ["SECRET_KEY"] = os.getenv("SECRET_KEY")
-os.environ["CLOUDINARY_URL"] = os.getenv("CLOUDINARY_URL")
-os.environ["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
-os.environ["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
